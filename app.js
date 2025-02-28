2e89c0b7938728dd71b231f83c1d1aa9c776e4fb
yinon2212 <yinonmegidesh2212@gmail.com>
2025-02-28 15:04:33 +0200

    Commit

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const twilioAPI = require('./src/sendSMS.js');
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://localhost:3001", "https://nevo-barbershop-lines.onrender.com/"],
+    origin: ["https://nevo-barbershop-lines.onrender.com"],
     methods: ["GET", "POST"],
   },
 });
