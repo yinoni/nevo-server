a39c89c5704881a71d6eb4ccb865ac4cfd93bba6
yinon2212 <yinonmegidesh2212@gmail.com>
2025-03-04 19:43:21 +0200

    FIxed malware

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const twilioAPI = require('./src/sendSMS.js');
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://localhost:3001", "https://nevo-barbershop-lines.onrender.com", "https://nevo-admin.onrender.com"],
+    origin: process.env.ORIGINS.split(',') || [],
     methods: ["GET", "POST"],
   },
 });
